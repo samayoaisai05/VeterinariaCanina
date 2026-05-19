627868fb0070609cccd5089fff0797cf01e114e8
Isai05 <samayoaisai05@gmail.com>
2026-05-19 13:32:23 -0600

    Integración de itnerfaz grafica de usuarios

---
+++ src/main/java/com/mycompany/peluqueriacanina/PeluqueriaCanina.java
@@ -0,0 +1,12 @@
+package com.mycompany.peluqueriacanina;
+
+import com.mycompany.peluqueriacanina.igu.Principal;
+
+public class PeluqueriaCanina {
+
+    public static void main(String[] args) {
+        Principal principal = new Principal();
+        principal.setVisible(true);
+        principal.setLocationRelativeTo(null);
+    }
+}
